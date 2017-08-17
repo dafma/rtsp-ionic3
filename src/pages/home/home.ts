4a1cf3e1568a879a774026ef56cbdb8f10d717ac
Pedro Luis Monroy Rodríguez <pedrolmr@MacBook-Air-de-Pedro.local>
2017-08-16 19:25:43 -0500

    reads

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var cordova:any;
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +12,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  SPlay(){
+    cordova.plugins.rtspVideo.play('rtsp://mpv.cdn3.bigCDN.com:554/bigCDN/definst/mp4:bigbuckbunnyiphone_400.mp4', function () {
+      console.log('Done Playing.');
+  }, function (e) {
+      console.error('Error: ' + e);
+  });
   }
 
 }
